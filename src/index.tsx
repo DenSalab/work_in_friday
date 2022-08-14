146d35bc91acb43472272e0e9fa8c06fdda8fe49
Denis <denis140292@gmail.com>
2022-08-14 13:13:53 +0300

    add applyMiddleware and HashRouter, remove dir .idea/

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import App from "./n1-main/m1-ui/App";
 import store from "./n1-main/m2-bll/store";
 import {Provider} from "react-redux";
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
--- src/n1-main/m2-bll/store.tsx
+++ src/n1-main/m2-bll/store.tsx
@@ -1,8 +1,10 @@
-import {createStore,combineReducers } from "redux";
-import {mainReducer} from "./mainReduser";
-
+import {createStore,combineReducers, applyMiddleware} from "redux";
+import {mainReducer} from "./mainReducer";
+import {loginReducer} from "./loginReducer";
+import thunk from "redux-thunk";
 const rootReducer = combineReducers({
     main: mainReducer,
+    login: loginReducer,
 })
-const store =  createStore(rootReducer)
+const store =  createStore(rootReducer, applyMiddleware(thunk))
 export default store;
