490760dae5f4d6e4d86bf5738f1d65890b87c837
JulieBenchuk <juliebenchuk@gmail.com>
2022-08-25 12:47:37 +0300

    added second baseURL (heroku)

--- src/main/dal/api.ts
+++ src/main/dal/api.ts
@@ -7,7 +7,7 @@ import {
 } from '../bll/loginReducer'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:7542/2.0/',
+  baseURL: /*'http://localhost:7542/2.0/' |*/ 'https://neko-back.herokuapp.com/2.0',
   withCredentials: true,
 })
 
